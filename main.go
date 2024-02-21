333ef9c7db55a745b2ca9e4b41e8bdf66cc70ffe
Gokul Prathin <gokulprathin8@gmail.com>
2024-02-21 00:48:04 -0500

    refactor logo to utils

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func (f *forcedVariant) Color(name fyne.ThemeColorName, _ fyne.ThemeVariant) col
 }
 
 func LightTheme(fallback fyne.Theme) fyne.Theme {
-	return &forcedVariant{Theme: fallback, variant: 1} // avoid import loops
+	return &forcedVariant{Theme: fallback, variant: 1}
 }
 
 func main() {
@@ -30,11 +30,16 @@ func main() {
 	app.Settings().SetTheme(&forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantLight})
 	window := app.NewWindow("Geoweaver")
 
-	text1 := canvas.NewText("1", color.Black)
+	logoWidth := 200
+	logoHeight := 200
+	geoweaverLogo := canvas.NewImageFromFile("assets/logo.png")
+	fixedSizeContainer := container.New(layout.NewFixedGridLayout(fyne.NewSize(float32(logoWidth), float32(logoHeight))), geoweaverLogo)
+
 	text2 := canvas.NewText("2", color.Black)
 	text3 := canvas.NewText("3", color.Black)
 	grid := container.New(layout.NewGridLayoutWithColumns(2), text1, text2, text3)
 	window.SetContent(grid)
+
 	window.ShowAndRun()
 
 }
---
+++ utils/setlogo.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+)
+
+func SetGeoweaverLogo() *fyne.Container {
+
+	LogoWidth := 200
+	LogoHeight := 200
+
+	geoweaverLogo := canvas.NewImageFromFile("../assets/logo.png")
+	logoContainer := container.New(layout.NewFixedGridLayout(fyne.NewSize(float32(LogoWidth), float32(LogoHeight))), geoweaverLogo)
+	return logoContainer
+
+}
