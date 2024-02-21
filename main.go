7e7f49a18c52e7e3ec305c75d8fc3bbb2407cc30
Gokul Prathin <gokulprathin8@gmail.com>
2024-02-21 01:59:25 -0500

    added java missing error

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"errors"
 	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 
@@ -32,6 +34,13 @@ func main() {
 	app.Settings().SetTheme(&forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantLight})
 	window := app.NewWindow("Geoweaver")
 	window.Canvas().Content().MinSize().Min(fyne.NewSize(800, 800))
+
+	// check for java
+	isJavaInstalled := utils.CheckJavaInstallation()
+	if isJavaInstalled {
+		errMsg := errors.New("Java is not installed. Please install Java to use to the application. (https://adoptopenjdk.net/releases.html)")
+		dialog.ShowError(errMsg, window)
+	}
 
 	text1 := utils.SetGeoweaverLogo()
 	text2 := canvas.NewText("2", color.Black)
